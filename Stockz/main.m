09c6692ffdbb2de8e604adf68c4a0ce093501717
Yasser Altamimi <yosy2010@gmail.com>
2018-09-14 20:41:16 +0300

    update

--- Stockz/main.m
+++ Stockz/main.m
@@ -10,8 +10,42 @@
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        
+        // create a mutable array for all the stock
+        NSMutableArray *stocks = [[NSMutableArray alloc] init];
+        
+        // create a mutable dictionarry for every stock info
+        NSMutableDictionary *stock;
+        
+        // init the dictionary
+        stock = [NSMutableDictionary dictionary];
+        
+        // add the key value pairs for the first stock
+        [stock setObject:@"AAPL"
+                  forKey:@"symbol"];
+        
+        [stock setObject:[NSNumber numberWithInt:200]
+                  forKey:@"shares"];
+        
+        // add the first stock as a dictionary to the first element of the array
+        [stocks addObject:stock];
+        
+        // reset the dictionary to make the second stock
+        stock = [NSMutableDictionary dictionary];
+        
+        // add the second stock with all its info to the dictionary to add to the array at the end
+        [stock setObject:@"GOOG"
+                  forKey:@"symbol"];
+        
+        [stock setObject:[NSNumber numberWithInt:160]
+                  forKey:@"shares"];
+        
+        [stocks addObject:stock];
+        
+        // write it all to the file in the specified path
+        [stocks writeToFile:@"/tmp/stocks.plist" atomically:YES];
+        
+        
     }
     return 0;
 }
